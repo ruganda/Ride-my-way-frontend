e2f0e520c3e4826d50d6e8b8dfdf941c17213da6
ruganda <mubaruganda@gmail.com>
2018-07-26 14:47:58 +0300

    implement create ride

--- UI/js/offer_ride.js
+++ UI/js/offer_ride.js
@@ -1,6 +1,6 @@
 
 document.getElementById('addRide').addEventListener('submit', addRide);
-
+let message = document.getElementById("message")
 function addRide(e){
     e.preventDefault();
 
@@ -8,14 +8,23 @@ function addRide(e){
     let destination = document.getElementById('destination').value;
     let date = document.getElementById('date').value;
 
-    fetch("http://127.0.0.1:5000/api/v1/rides/", {
+    fetch("http://127.0.0.1:5000/api/v2/users/rides/", {
       method:'POST',
       headers: {
+        'Authorization':"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VybmFtZSI6ImpvaG5ydWdhbmRhIiwiZXhwIjoxNTMzMDU5ODI1fQ.p17pjjWxMdR26qpCJo9zNbsaoDOFBwEVI1abbrx4Q2U",
         'Accept': 'application/json, text/plain, */*',
         'Content-type':'application/json'
       },
       body:JSON.stringify({origin:origin, destination:destination, date:date})
     })
     .then((res) => res.json())
-    .then((data) => console.log(data))
+    .then((data) => {
+      console.log(data);
+      "error" in data ? message.innerHTML = ` <div> ${data.message} </div>` : message.innerHTML = ` <p> ${data.message}</p>` ;
+      message.style.display = "block";
+      // message.innerHTML=data.message;
+      message.innerHTML  = `<div >${data.message}</div>`
+      message.style.display = "block";
+
+    })
   }
