6217d5127073e1780f7a1cde2797312cc3c07200
ruganda <mubaruganda@gmail.com>
2018-07-26 14:47:27 +0300

    fetch all rides

--- UI/js/all_rides.js
+++ UI/js/all_rides.js
@@ -1,4 +1,15 @@
 "use strict";
+// const ride = document.getElementById('card')
+const saveIdOnLocalStorage = (e) => {
+    const { id } = e.target;
+  
+    localStorage.setItem('id', `${parseInt(id, 10)}`);
+    window.location.href ='./ride_details.html';
+  };
+
+  
+
+ 
 
 document.getElementById('all_rides').addEventListener("load", getRides());
 
@@ -6,17 +17,18 @@ function getRides(){
     fetch(" http://127.0.0.1:5000/api/v2/rides/", {
     //   mode:'cors',
       headers: {
-        'Authorization':"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VybmFtZSI6Im11YmFydWdhbmRhIiwiZXhwIjoxNTMyNTc1MDg1fQ.Z5xF-CJPWScudG4gKH47zwJJ0BDM1RAEdfHak56h3HU",
-        'Accept': 'application/json, text/plain, */*',
+        'Authorization':localStorage.getItem("access_token"),
+        'Accept': 'application/json',
         'Content-type':'application/json'
       }
     })
     .then((response) =>response.json())
     .then((data) => {
         console.log(data);
+        
         // document.getElementById("card").innerHTML=data;
         let output = `<div class="card">
-                            <div id="ride-info" href="ride_details.html">
+                            <div id="ride-info" >
                                 <p>Id</p>
                                 <p>Origin</p>
                                 <p>Travel date </p>
@@ -26,23 +38,26 @@ function getRides(){
                       </div>`
         data.forEach(function(ride){
             output+=
+            
             `
-            <div class="card" id ="card">
-                <a href="ride_details.html">
-                    <div class="ride-info">
-                        <p> ${ride.id}</p>
-                        <p>${ride.origin}</p>
-                        <p>${ride.date}</p>
-                        <p>${ride.destination}</p>
-                        <p>${ride.driver}</p>
-                    </div>
-                </a>
+            <div class="card" id ="card">  
+                
+                <div class="ride-info">
+                    <p> ${ride.id}</p>
+                    <p>${ride.origin}</p>
+                    <p>${ride.date}</p>
+                    <p>${ride.destination}</p>
+                    <p>${ride.driver}</p>
+                </div>   
+                
             </div>
-            `;      
+            `
+            localStorage.setItem('id',ride.id);      
         });
         document.getElementById("row").innerHTML = output;
-        // console.log(output);
+        document.getElementById('card').addEventListener('click', saveIdOnLocalStorage, false);
+        
     })
-    // .catch((err) =>console.log(err))
+    .catch((err) =>console.log(err))
     
 }
