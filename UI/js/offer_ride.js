bda026d83b037d4cba9d76afa022349e099af147
Ruganda Mubarak <rugandamubarak@Rugandas-MacBook-Pro.local>
2018-08-13 23:44:22 +0300

    Add guards to routes

--- UI/js/offer_ride.js
+++ UI/js/offer_ride.js
@@ -11,7 +11,7 @@ function addRide(e){
       method:'POST',
       headers: {
         'Authorization':localStorage.getItem("access_token"),
-        'Accept': 'application/json, text/plain, */*',
+        'Accept': 'application/json',
         'Content-type':'application/json'
       },
       body:JSON.stringify({origin:origin, destination:destination, date:date})
@@ -19,7 +19,9 @@ function addRide(e){
     .then((res) => res.json())
     .then((data) => {
       console.log(data);
-      if (data.message === "You offered a ride successfully."){
+      if (data.message === "Token is invalid!"){
+        window.location.href = './login.html'
+      }else if (data.message === "You offered a ride successfully."){
         success.innerHTML = data.message
         window.location.href = './all_rides.html'
       }else{
